a815dacb628ecf736c3ae6b1abee633dba9b8d6f
Emanuel Martín <33545553+emanuelmartin@users.noreply.github.com>
2025-03-06 17:03:19 -0600

    Cambios

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -4,39 +4,57 @@ export const About = (props) => {
   return (
     <div id="about">
       <div className="container">
-        <div className="row">
-          <div className="col-xs-12 col-md-6">
-            {" "}
-            <img src="img/about.jpg" className="img-responsive" alt="" />{" "}
+        {/* First Section: Main Image & Paragraph */}
+        <div className="row align-items-center">
+          <div className="col-xs-12 col-md-6 text-center">
+            <img src="img/about.jpg" className="img-responsive" alt="About us" />
           </div>
           <div className="col-xs-12 col-md-6">
             <div className="about-text">
               <h2>Quiénes somos</h2>
               <p>{props.data ? props.data.paragraph : "loading..."}</p>
+            </div>
+          </div>
+        </div>
+        <br />
+        <br />
+
+        {/* Second Section: Misión, Visión, Valores + Another Image */}
+        <div className="row align-items-center mt-5">
+          <div className="col-xs-12 col-md-6">
+            <div className="about-text">
+              <h3>Misión</h3>
+              <p>{props.data ? props.data.mission : "loading..."}</p>
+
+              <h3>Visión</h3>
+              <p>{props.data ? props.data.vision : "loading..."}</p>
+
               <h3>Valores</h3>
               <div className="list-style">
-                <div className="col-lg-6 col-sm-6 col-xs-12">
-                  <ul>
-                    {props.data
-                      ? props.data.Why.map((d, i) => (
-                          <li key={`${d}-${i}`}>{d}</li>
-                        ))
-                      : "loading"}
-                  </ul>
-                </div>
-                <div className="col-lg-6 col-sm-6 col-xs-12">
-                  <ul>
-                    {props.data
-                      ? props.data.Why2.map((d, i) => (
-                          <li key={`${d}-${i}`}> {d}</li>
-                        ))
-                      : "loading"}
-                  </ul>
+                <div className="row">
+                  <div className="col-lg-6 col-sm-6 col-xs-12">
+                    <ul>
+                      {props.data
+                        ? props.data.Why.map((d, i) => <li key={`${d}-${i}`}>{d}</li>)
+                        : "loading"}
+                    </ul>
+                  </div>
+                  <div className="col-lg-6 col-sm-6 col-xs-12">
+                    <ul>
+                      {props.data
+                        ? props.data.Why2.map((d, i) => <li key={`${d}-${i}`}>{d}</li>)
+                        : "loading"}
+                    </ul>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
+          {/* Switched Image to the Right */}
+          <div className="col-xs-12 col-md-6 text-center">
+          <img src="img/about.jpg" className="img-responsive" alt="About us" />
+          </div>
+        </div> 
       </div>
     </div>
   );
